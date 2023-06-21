0dd2e6b3839ae6fe267fe0de93ab1af9bc4def44
Perchun Pak <git@perchun.it>
2023-06-21 17:36:08 +0200

    Remove home route

--- src/routes/__init__.py
+++ src/routes/__init__.py
@@ -8,17 +8,8 @@ from src.config import Config
 
 def register_all_routers(app: fastapi.FastAPI) -> None:
     """Register all routers to the application."""
-    register_home_route(app)
     for version in Config().api_versions:
         module = importlib.import_module(f"src.routes.versions.v{version}")
         router = getattr(module, f"V{version}ApiRouter")()
         router.register()
         app.include_router(router.router)
-
-
-def register_home_route(app: fastapi.FastAPI) -> None:
-    """Register home route that redirects to API docs."""
-
-    @app.get("/")
-    def home() -> fastapi.responses.RedirectResponse:
-        return fastapi.responses.RedirectResponse(url="https://statusmc.perchun.it/api")
