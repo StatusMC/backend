ede15b9af81d7f7fb5d6616e800f864f0589fd10
Perchun Pak <git@perchun.it>
2023-07-10 14:45:54 +0000

    Return address as string, if it's invalid

--- src/models/v1/offline.py
+++ src/models/v1/offline.py
@@ -10,7 +10,7 @@ from src.models.v1.shared import AddressInResponse, BaseStatusResponse
 
 class OfflineStatusResponse(BaseStatusResponse):
     online: t.Literal[False]
-    address: AddressInResponse
+    address: t.Union[AddressInResponse, str]
     error: MCStatusException
 
     @classmethod
@@ -21,17 +21,30 @@ class OfflineStatusResponse(BaseStatusResponse):
         *,
         is_java: bool,
     ) -> te.Self:
-        address = (
-            server.address
-            if not isinstance(server, str)
-            else mcstatus.address.Address.parse_address(server, default_port=25565 if is_java else 19132)
-        )
+        address = cls._parse_address(server, is_java)
 
         return cls(
             online=False,
-            address=AddressInResponse(
-                host=address.host,
-                port=address.port,
+            address=(
+                address
+                if isinstance(address, str)
+                else AddressInResponse(
+                    host=address.host,
+                    port=address.port,
+                )
             ),
             error=error,
         )
+
+    @staticmethod
+    def _parse_address(
+        server: t.Union[mcstatus.JavaServer, mcstatus.BedrockServer, str],
+        is_java: bool,
+    ) -> t.Union[mcstatus.address.Address, str]:
+        if not isinstance(server, str):
+            return server.address
+
+        try:
+            return mcstatus.address.Address.parse_address(server, default_port=25565 if is_java else 19132)
+        except ValueError:  # the address is invalid
+            return server
