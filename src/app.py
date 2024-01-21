7a5ec26df63a7d5adecbfc91d993d0f7bc662266
PerchunPak <git@perchun.it>
2024-01-21 01:09:04 +0100

    Enable Sentry

--- src/app.py
+++ src/app.py
@@ -4,7 +4,9 @@ import uvicorn
 
 import src.middlewares as middlewares
 import src.routes as routes
+import src.utils as utils
 
+utils.start_sentry()
 app = fastapi.FastAPI()
 middlewares.register_middlewares(app)
 routes.register_all_routers(app)
